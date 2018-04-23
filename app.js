7d8257d0e26c710c360716ae82df57685463b3c4
Lochan Chhetri <lochan.chhetri@macys.com>
2018-04-23 11:19:55 -0700

    Update api key

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/macys/api/discover/v1/search', (req, res) => {
-    const url = 'https://api.mcom-011.tbe.zeus.fds.com/discover/v1/search?keyword=' + req.query.keyword + '&size=small&requestFacets=true&requestProducts=true&pathname=/shop/search&_application=MEW&_deviceType=PHONE&_navigationType=SEARCH&assortment=SITE&_regionCode=US&_customerState=GUEST';
+    const url = 'https://api.macys.com/discover/v1/search?keyword=' + req.query.keyword + '&size=small&requestFacets=true&requestProducts=true&pathname=/shop/search&_application=MEW&_deviceType=PHONE&_navigationType=SEARCH&assortment=SITE&_regionCode=US&_customerState=GUEST';
 
     request({
         url: url,
         headers: {
             'Accept': 'application/json',
-            'x-macys-webservice-client-id': '8pa42ea2hhh5we6x3jsqy2d7'
+            'x-macys-webservice-client-id': '95345ypwu55hyvs6em3uv5mg'
         }
     },
     function (error, response, body) {
