b4237e54db0fe0c52f9a953211a45722c1bd4d64
Lochan Chhetri <lochan.chhetri@macys.com>
2018-04-23 12:16:43 -0700

    Point endpoint to m.macys.com instead of api.macys.com

--- app.js
+++ app.js
@@ -5,18 +5,17 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/macys/api/discover/v1/search', (req, res) => {
-    const url = 'https://api.macys.com/discover/v1/search?keyword=' + req.query.keyword + '&size=small&requestFacets=true&requestProducts=true&pathname=/shop/search&_application=MEW&_deviceType=PHONE&_navigationType=SEARCH&assortment=SITE&_regionCode=US&_customerState=GUEST';
+    const url = 'https://m.macys.com/api/discover/v1/search?keyword=' + req.query.keyword + '&size=small&requestFacets=true&requestProducts=true&pathname=/shop/search&_application=MEW&_deviceType=PHONE&_navigationType=SEARCH&assortment=SITE&_regionCode=US&_customerState=GUEST';
 
     request({
         url: url,
-        headers: {
-            'Accept': 'application/json',
-            'x-macys-customer-id': 'testclient_1.0_kweu3w323a'
-        }
+        // headers: {
+        //     'Accept': 'application/json',
+        //     'x-macys-customer-id': 'testclient_1.0_kweu3w323a'
+        // }
     },
     function (error, response, body) {
         if(!error && response.statusCode === 200) {
-            console.log(response.request.headers);
             res.json(JSON.parse(body));
         } else {
             console.log(error);
