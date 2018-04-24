6519a571cbc3b25b67e825d4a9fd7daf90240c2f
Lochan Chhetri <lochan.chhetri@macys.com>
2018-04-24 13:09:06 -0700

    request debug

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const request = require('request');
+require('request').debug = true
 const app = express();
+
 
 const port = process.env.PORT || 3000;
 
 app.get('/macys/api/discover/v1/search', (req, res) => {
     request({
-        url: 'http://api.macys.com/discover/v1/search?keyword=' + req.query.keyword + '&size=small&requestFacets=true&requestProducts=true&pathname=/shop/search&_application=MEW&_deviceType=PHONE&_navigationType=SEARCH&assortment=SITE&_regionCode=US&_customerState=GUEST',
+        url: 'https://api.macys.com/discover/v1/search?keyword=' + req.query.keyword + '&size=small&requestFacets=true&requestProducts=true&pathname=/shop/search&_application=MEW&_deviceType=PHONE&_navigationType=SEARCH&assortment=SITE&_regionCode=US&_customerState=GUEST',
         headers: {
             'Accept': 'application/json',
             'x-macys-webservice-client-id': 'wzuuhtwrrpegszaehcu6ey2n',
