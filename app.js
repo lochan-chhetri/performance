1cb2d8bb91b8f8454e370f6e9c7ede839c380b9e
Lochan Chhetri <lochan.chhetri@macys.com>
2018-04-24 13:37:24 -0700

    more debugs

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 3000;
 
 app.get('/macys/api/discover/v1/search', (req, res) => {
     request({
-        url: 'https://api.macys.com/discover/v1/search?keyword=' + req.query.keyword + '&size=small&requestFacets=true&requestProducts=true&pathname=/shop/search&_application=MEW&_deviceType=PHONE&_navigationType=SEARCH&assortment=SITE&_regionCode=US&_customerState=GUEST',
+        url: 'https://origin-api.macys.com/discover/v1/search?keyword=' + req.query.keyword + '&size=small&requestFacets=true&requestProducts=true&pathname=/shop/search&_application=MEW&_deviceType=PHONE&_navigationType=SEARCH&assortment=SITE&_regionCode=US&_customerState=GUEST',
         headers: {
             'Accept': 'application/json',
             'x-macys-webservice-client-id': 'wzuuhtwrrpegszaehcu6ey2n',
